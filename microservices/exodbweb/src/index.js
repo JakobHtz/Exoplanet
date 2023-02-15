4487e1935b1322a47e2c2af3057ccffc0e406648
Jakob Heitzmann <jakob.heitzmann@web.de>
2023-02-15 08:02:07 +0100

    feat(exodbweb): Adds DB init at startup

--- microservices/exodbweb/src/index.js
+++ microservices/exodbweb/src/index.js
@@ -4,18 +4,29 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const events = require('./events');
 
+const database = 'exodb';
+
 const addr = process.env.MARIADB_ADDR;
 const hostaddr = typeof addr == "undefined"||addr==""?'localhost':addr;
 
 const connection = mysql.createConnection({
   host     : hostaddr,
   user     : 'root',
-  password : 'test',
-  database : 'exodb'
+  password : 'test'
 });
 
 console.log("Connecting to " + hostaddr);
 connection.connect();
+
+connection.query("CREATE DATABASE IF NOT EXISTS " + database + ";");
+console.log("Database created or exists.");
+
+connection.query('USE ' + database + ";");
+console.log("Database used: " + database);
+
+connection.query("CREATE TABLE IF NOT EXISTS Robot(RID INT AUTO_INCREMENT,x_pos INT,y_pos INT,dir VARCHAR(10),PRIMARY KEY(RID));");
+connection.query("CREATE TABLE IF NOT EXISTS PlanetData(DID INT AUTO_INCREMENT,RID INT,planet VARCHAR(20),timestamp DATETIME,temp DOUBLE,ground VARCHAR(10),x_pos INT,y_pos INT,PRIMARY KEY(DID),FOREIGN KEY(RID) REFERENCES Robot(RID));");
+console.log("Tables created or exist.");
 
 const port = process.env.PORT || 8081;
 
